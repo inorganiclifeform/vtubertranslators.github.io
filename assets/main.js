5fdf0995d46781c58c4e51eae298da328d13aa56
Ranie Santos <ranietots32@gmail.com>
2020-08-03 12:11:35 +0800

    Links open in new tab and go to /videos page

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 function createList(channels) {
     return channels.map((channel) => `
-        <a href="${channel.url}" class="channel">
+        <a href="${channel.url}/videos" class="channel" target="_blank">
             ${channel.name}
         </a>
     `).join('');
